373b0c48684db211b864f004dd4011144f9c94ac
dlii0086 <dli0086@student.monash.edu>
2024-07-09 16:26:00 +0800

    Change driver with the latest version Change code for the latest version

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.chrome.service import Service
 import time
-import datetime
 import schedule
 
 login_link="http://1.1.1.5/"
 utmid = "" # username
 password = "" # password
 
-browser_type = "firefox" # edge or firefox
+browser_type = "chrome" # edge or firefox
 
 if browser_type == "edge" :
     driver = "driver/msedgedriver.exe"
@@ -19,38 +19,36 @@ elif browser_type =='firefox' :
     driver = "/snap/bin/firefox.geckodriver"    
     
 elif browser_type == "chrome" :
-    driver = ""
+    driver = "driver/chromedriver.exe"
 
 id_utmid = "une"
 id_password = "pass"
 id_submit = "btlogin"
-
 def login():
-    browser = None  # Initialize browser variable
+    if browser_type == "edge" :
+        browser =  webdriver.Edge(service=Service(driver))
+    elif browser_type =='firefox' : 
+        browser =  webdriver.Firefox(service=Service(driver))
+    elif browser_type =='chrome' : 
+        browser =  webdriver.Chrome(service=Service(driver))
+
+    browser.get((login_link))	
 
     try:
-        if browser_type == "edge":
-            browser = webdriver.Edge(service=EdgeService(driver))
-        elif browser_type == 'firefox':
-            browser = webdriver.Firefox(service=FirefoxService(driver))
-
-        browser.get(login_link)
-
-        browser.find_element(by=By.NAME, value=id_utmid).send_keys(utmid)
+        browser.find_element(by=By.NAME, value=id_utmid).send_keys(utmid)		
         browser.find_element(by=By.NAME, value=id_password).send_keys(password)
-        browser.execute_script("arguments[0].click();", browser.find_element(By.ID, "tnc"))
+        browser.execute_script("arguments[0].click();", browser.find_element(By.ID,"tnc"))
         browser.find_element(by=By.NAME, value=id_submit).click()
-
+        
         print("Login Success")
         time.sleep(3)
+        browser.quit()
 
-    except Exception as e:
-        print(f"Error: {e}")
-    
-    finally:
-        if browser:
-            browser.quit()
-
+    except Exception:
+        print("Error")
+        time.sleep(3)
+        browser.quit()
+     
 schedule.every().day.at("02:10").do(login) #time for login
 
 while True:
